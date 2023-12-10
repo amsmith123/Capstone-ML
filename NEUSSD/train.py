790f97da573b2a24449117c1efcb9494ffabe30f
Arjun <arjunmsmith@gmail.com>
2023-12-10 12:10:16 -0500

    yolov8 large model

--- NEUSSD/train.py
+++ NEUSSD/train.py
@@ -5,14 +5,14 @@ import os
 # Get directory path for the file
 pd = os.path.abspath(os.path.dirname(__file__))
 
-# Load a COCO-pretrained YOLOv8s model
-model = YOLO('yolov8s-seg.pt')
+# Load a COCO-pretrained YOLOv8l (large) model
+model = YOLO('yolov8l-seg.pt')
 
-# Train the model using the dataset for 20 epochs
-results = model.train(data=f'{pd}/Data/data.yaml', epochs=20)
+# Train the model using the dataset for 100 epochs
+results = model.train(data=f'{pd}/Data/data.yaml', epochs=100)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
 
-# Export the model to ONNX format, save as model.onnx
-success = model.export(format='onnx', path=f'{pd}/model.onnx')
+# Export the model to ONNX format
+success = model.export(format='onnx')
